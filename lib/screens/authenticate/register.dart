90ecfdf1d9469cf761075f4d35794d59ce8d95ee
shreya <shreyamrao@gmail.com>
2023-09-01 03:06:50 +0530

    changed frontend to rajivs code

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -48,71 +48,192 @@ class _RegisterState extends State<Register> {
               decoration: const BoxDecoration(
                 image: DecorationImage(
                     image: AssetImage("assets/register.png"),
-                    fit: BoxFit.cover),
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topCenter),
               ),
-              // body: Container(
-              // padding:
-              // const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    const SizedBox(height: 20.0),
-                    TextFormField(
-                        decoration: textInputDecoration.copyWith(
-                          hintText: 'Email',
-                          hintStyle: const TextStyle(color: Colors.black45),
+              child: Scaffold(
+                backgroundColor: Colors.transparent,
+                body: Stack(
+                  children: [
+                    // Container(
+                    //   padding: const EdgeInsets.only(left: 35, top: 30),
+                    //   child: const Text(
+                    //     'Create\nAccount',
+                    //     style: TextStyle(color: Colors.white, fontSize: 33),
+                    //   ),
+                    // ),
+                    SingleChildScrollView(
+                      child: Container(
+                        padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.05),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin:
+                                  const EdgeInsets.only(left: 35, right: 35),
+                              child: Column(
+                                children: [
+                                  // body: Container(
+                                  // padding:
+                                  // const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                                  Form(
+                                    key: _formKey,
+                                    child: Column(
+                                      children: <Widget>[
+                                        const SizedBox(height: 30.0),
+                                        TextFormField(
+                                            style: const TextStyle(
+                                                color: Colors.black),
+                                            decoration:
+                                                textInputDecoration.copyWith(
+                                              fillColor: Colors.grey.shade300,
+                                              filled: true,
+                                              hintText: 'Email',
+                                              hintStyle: const TextStyle(
+                                                  color: Colors.black45),
+                                              enabledBorder: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(10),
+                                                borderSide: const BorderSide(
+                                                  color: Colors.black,
+                                                ),
+                                              ),
+                                              focusedBorder: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(10.0),
+                                                borderSide: const BorderSide(
+                                                  color: Colors.blue,
+                                                  width: 2.0,
+                                                ),
+                                              ),
+                                            ),
+                                            validator: (val) => val!.isEmpty
+                                                ? 'Enter your email'
+                                                : null,
+                                            onChanged: (val) {
+                                              setState(() => email = val);
+                                            }),
+                                        const SizedBox(height: 30.0),
+                                        TextFormField(
+                                          style: const TextStyle(
+                                              color: Colors.black),
+                                          decoration:
+                                              textInputDecoration.copyWith(
+                                            fillColor: Colors.grey.shade300,
+                                            filled: true,
+                                            hintText: 'Password',
+                                            hintStyle: const TextStyle(
+                                                color: Colors.black45),
+                                            enabledBorder: OutlineInputBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              borderSide: const BorderSide(
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                            focusedBorder: OutlineInputBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(10.0),
+                                              borderSide: const BorderSide(
+                                                color: Colors.blue,
+                                                width: 2.0,
+                                              ),
+                                            ),
+                                          ),
+                                          obscureText: true,
+                                          validator: (val) => val!.length < 8
+                                              ? 'Password should be min 8 chars'
+                                              : null,
+                                          onChanged: (val) {
+                                            setState(() => password = val);
+                                          },
+                                        ),
+                                        // ElevatedButton(
+                                        //     style: ElevatedButton.styleFrom(
+                                        //       backgroundColor: Colors.blue[900],
+                                        //     ),
+                                        //     child: const Text(
+                                        //       'Register',
+                                        //       style: TextStyle(color: Colors.white),
+                                        //     ),
+                                        //     onPressed: () async {
+                                        //       if (_formKey.currentState!.validate()) {
+                                        //         setState(() => loading = true);
+                                        //         dynamic result =
+                                        //             await _auth.registerWithEmailAndPassword(
+                                        //                 email, password);
+                                        //         if (result == null) {
+                                        //           setState(() {
+                                        //             error = 'Please supply a valid email';
+                                        //             loading = false;
+                                        //           });
+                                        //         }
+                                        //       }
+                                        //     }),
+                                        const SizedBox(height: 100.0),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            const Text(
+                                              'Register',
+                                              style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 27,
+                                                  fontWeight: FontWeight.w700),
+                                            ),
+                                            CircleAvatar(
+                                              radius: 30,
+                                              backgroundColor:
+                                                  const Color(0xff4c505b),
+                                              child: IconButton(
+                                                  color: Colors.white,
+                                                  onPressed: () async {
+                                                    if (_formKey.currentState!
+                                                        .validate()) {
+                                                      setState(
+                                                          () => loading = true);
+                                                      dynamic result = await _auth
+                                                          .registerWithEmailAndPassword(
+                                                              email, password);
+                                                      if (result == null) {
+                                                        setState(() {
+                                                          error =
+                                                              'Please supply a valid email';
+                                                          loading = false;
+                                                        });
+                                                      }
+                                                    }
+                                                  },
+                                                  icon: const Icon(
+                                                    Icons.arrow_forward,
+                                                  )),
+                                            )
+                                          ],
+                                        ),
+
+                                        const SizedBox(height: 12.0),
+                                        Text(
+                                          error,
+                                          style: const TextStyle(
+                                              color: Colors.red,
+                                              fontSize: 14.0),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  //container
+                                ], //children
+                              ),
+                            ),
+                          ],
                         ),
-                        validator: (val) =>
-                            val!.isEmpty ? 'Enter your email' : null,
-                        onChanged: (val) {
-                          setState(() => email = val);
-                        }),
-                    const SizedBox(height: 20.0),
-                    TextFormField(
-                      decoration: textInputDecoration.copyWith(
-                        hintText: 'Password',
-                        hintStyle: const TextStyle(color: Colors.black45),
                       ),
-                      obscureText: true,
-                      validator: (val) => val!.length < 8
-                          ? 'Password should be min 8 chars'
-                          : null,
-                      onChanged: (val) {
-                        setState(() => password = val);
-                      },
-                    ),
-                    const SizedBox(height: 20.0),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue[900],
-                        ),
-                        child: const Text(
-                          'Register',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            setState(() => loading = true);
-                            dynamic result = await _auth
-                                .registerWithEmailAndPassword(email, password);
-                            if (result == null) {
-                              setState(() {
-                                error = 'Please supply a valid email';
-                                loading = false;
-                              });
-                            }
-                          }
-                        }),
-                    const SizedBox(height: 12.0),
-                    Text(
-                      error,
-                      style: const TextStyle(color: Colors.red, fontSize: 14.0),
                     ),
                   ],
                 ),
               ),
-            ),
-          );
+            ));
   }
 }
